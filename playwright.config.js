fc2304c4ab28873512efc1389b29d3ef2e7abfe7
Mitch Winkel-Davis <mitch@mitchwd.com>
2025-03-16 13:38:50 +1100

    Fix config to launch dev server

--- playwright.config.js
+++ playwright.config.js
@@ -72,10 +72,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run preview',
-  //   url: 'http://127.0.0.1:4173',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:5173',
+    reuseExistingServer: !process.env.CI,
+  },
 });
 
