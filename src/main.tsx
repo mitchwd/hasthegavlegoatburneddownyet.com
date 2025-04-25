afdd3d728609f755aca7bfe5fbc8d9c6d6968144
Mitch Winkel-Davis <mitch@mitchwd.com>
2025-04-25 17:31:29 +1000

    Move Vercel packages one level higher so they don't pollute test logs

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
@@ -6,5 +8,7 @@ import './index.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <SpeedInsights />
+    <Analytics />
   </StrictMode>
 );
