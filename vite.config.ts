269d3697819d5c776f5d80359e1b5a554d334409
Mitch Winkel-Davis <mitch@mitchwd.com>
2026-06-26 21:42:13 +1000

    Deps bump - June 2026 (#153)

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,13 @@
-/// <reference types="vitest/config" />
-
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import { playwright } from '@vitest/browser-playwright';
 import { defineConfig } from 'vite';
-import vercel from 'vite-plugin-vercel';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), vercel(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 4173,
   },
-  test: {
-    name: 'component',
-    browser: {
-      enabled: true,
-      provider: playwright(),
-      instances: [
-        {
-          browser: 'chromium',
-          setupFiles: './src/setup-tests.ts',
-        },
-      ],
-    },
-    globals: true,
-    css: true,
-    coverage: {
-      provider: 'istanbul',
-      include: ['src/**/*.{ts,tsx}'],
-      exclude: ['src/main.tsx', 'coverage/**', 'dist/**'],
-      reporter: ['html', 'text', 'json-summary', 'json', 'lcovonly'],
-      reportOnFailure: true,
-    },
-    // typecheck: {
-    //   enabled: true,
-    // },
-    includeTaskLocation: true,
-  },
-  vercel: {},
   optimizeDeps: {
     exclude: ['chromium-bidi', 'playwright'],
   },
