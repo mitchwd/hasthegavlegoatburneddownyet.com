1b9b6e48f263b741b9731a9016f212080608b9a2
Mitch Winkel-Davis <mitch@mitchwd.com>
2026-06-26 20:47:12 +1000

    Upgrade dependencies, reduce package minimum age

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-/// <reference types="vitest/config" />
-
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import { playwright } from '@vitest/browser-playwright';
 import { defineConfig } from 'vite';
-import vercel from 'vite-plugin-vercel';
+import vercel from 'vite-plugin-vercel/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,33 +9,32 @@ export default defineConfig({
   server: {
     port: 4173,
   },
-  test: {
-    name: 'component',
-    browser: {
-      enabled: true,
-      provider: playwright(),
-      instances: [
-        {
-          browser: 'chromium',
-          setupFiles: './src/setup-tests.ts',
-        },
-      ],
-    },
-    globals: true,
-    css: true,
-    coverage: {
-      provider: 'istanbul',
-      include: ['src/**/*.{ts,tsx}'],
-      exclude: ['src/main.tsx', 'coverage/**', 'dist/**'],
-      reporter: ['html', 'text', 'json-summary', 'json', 'lcovonly'],
-      reportOnFailure: true,
-    },
-    // typecheck: {
-    //   enabled: true,
-    // },
-    includeTaskLocation: true,
-  },
-  vercel: {},
+  //   test: {
+  //   name: 'component',
+  //   browser: {
+  //     enabled: true,
+  //     provider: playwright(),
+  //     instances: [
+  //       {
+  //         browser: 'chromium',
+  //         setupFiles: './src/setup-tests.ts',
+  //       },
+  //     ],
+  //   },
+  //   globals: true,
+  //   css: true,
+  //   coverage: {
+  //     provider: 'istanbul',
+  //     include: ['src/**/*.{ts,tsx}'],
+  //     exclude: ['src/main.tsx', 'coverage/**', 'dist/**'],
+  //     reporter: ['html', 'text', 'json-summary', 'json', 'lcovonly'],
+  //     reportOnFailure: true,
+  //   },
+  //   // typecheck: {
+  //   //   enabled: true,
+  //   // },
+  //   includeTaskLocation: true,
+  // },
   optimizeDeps: {
     exclude: ['chromium-bidi', 'playwright'],
   },
