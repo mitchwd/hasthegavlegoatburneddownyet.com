39b4379184b6897989e70aa5b8c0ebd432b318c8
Mitch Winkel-Davis <mitch@mitchwd.com>
2026-06-26 21:36:59 +1000

    Remove vite-plugin-vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import vercel from 'vite-plugin-vercel/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), vercel(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 4173,
   },
